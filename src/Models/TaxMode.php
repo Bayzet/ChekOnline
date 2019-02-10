14123d0b926e72d6a950ae9202a3e390186abc34
Bayzet Tlyupov <baizet01@gmail.com>
2019-02-10 18:11:14 +0300

    Полная перераюотка структуры Внесённые изменения: - Сделал реализацию методов через интерфейс - Добавил классы моделей для свойств со значениями из таблиц - Доработал метод добавления еденицы товара в чек

---
+++ src/Models/TaxMode.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bayzet\ChekOnline\Model;
+
+/**
+ * Тип системы налогообложения (Таблица 3)
+ */
+class TaxMode
+{
+
+    /** @const int Общая  */
+    const OVERALL = 1;
+    
+    /** @const int Упрощённая доход  */
+    const SIMPLIFIED_INCOME = 2;
+    
+    /** @const int Упрощённая доход минус расход  */
+    const SIMPLIFIED_INCOME_MINUS_EXPENSE = 4;
+    
+    /** @const int Единый налог на вменённый доход  */
+    const ENVD = 8;
+    
+    /** @const int Единый сельскохозяйственный налог  */
+    const ESN = 16;
+    
+    /** @const int Патентная система налогообложения  */
+    const PSN = 32;
+
+}
